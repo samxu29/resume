f2e84cb60995519d971fc4188d5f157f9e6f23b0
Sam Xu <22229980+samxu29@users.noreply.github.com>
2023-11-15 14:43:55 -0500

    update

--- pdf2png.py
+++ pdf2png.py
@@ -1,32 +1,32 @@
-# pip3 install PyMuPDF Pillow
-
-import fitz  # PyMuPDF
+import fitz 
 from PIL import Image
+import argparse
 
 def convert_pdf_to_png(pdf_path, output_folder):
-    # Open the PDF file using PyMuPDF
+    
     pdf_document = fitz.open(pdf_path)
     
-    # Iterate through each page in the PDF
     for page_number in range(len(pdf_document)):
-        # Get a specific page
+        
         page = pdf_document.load_page(page_number)
         
-        # Convert the page to an image
         image = page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
         
-        # Create a Pillow image from the raw image data
         pillow_image = Image.frombytes("RGB", [image.width, image.height], image.samples)
         
-        # Save the image as a PNG file
-        image_filename = f"{output_folder}/page_{page_number + 1}.png"
+        image_filename = f"{output_folder}/{pdf_path}_{page_number + 1}.png"
         pillow_image.save(image_filename, "PNG")
 
-    # Close the PDF file
     pdf_document.close()
 
-if __name__ == "__main__":
-    input_pdf_path = "shengkaixu_2023resume.pdf"  # Replace with the path to your PDF file
-    output_folder = "pdf2png"  # Output folder where PNG images will be saved
+def main():
+    parser = argparse.ArgumentParser(description="Convert PDF to PNG")
+    parser.add_argument("pdf_path", help="Path to the input PDF file")
+    parser.add_argument("output_folder", help="Path to the output folder where PNGs will be saved")
 
-    convert_pdf_to_png(input_pdf_path, output_folder)
+    args = parser.parse_args()
+
+    convert_pdf_to_png(args.pdf_path, args.output_folder)
+
+if __name__ == "__main__":
+    main()
